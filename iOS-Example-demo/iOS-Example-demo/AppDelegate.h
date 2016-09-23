a5524ae1083605d0eab07914fdad4c29cdcc1f9e
Arvin <yasir86@126.com>
2016-09-23 15:13:17 +0800

    initialization project

---
+++ iOS-Example-demo/iOS-Example-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Example-demo
+//
+//  Created by Arvin on 16/9/23.
+//  Copyright © 2016年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-Example-demo/iOS-Example-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-Example-demo
+//
+//  Created by Arvin on 16/9/23.
+//  Copyright © 2016年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
